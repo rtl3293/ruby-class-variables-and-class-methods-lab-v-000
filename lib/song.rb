41c5f93ab0c0b2f23da572b118787fa61068e054
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-29 03:18:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    if @@genres.include?(@genre)
-      @@genres << @genre
-    end
+    @@genres << @genre
   end
 
   def self.count
