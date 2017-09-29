144c687bb5fc795460eee8e06a33831f7990b916
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-29 03:05:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
-  attr_accessor :name
-  attr_accessor :artist
-  attr_accessor :genre
+
   @@count = 0
   @@artists = []
   @@genres = []
 
 
   def initialize(name, artist, genre)
+    attr_accessor :name
+    attr_accessor :artist
+    attr_accessor :genre
     @@count += 1
     @@genres.include?(:genre) ? @@genres << :genre : return
   end
